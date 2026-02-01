d89a2b3a72f717eb60abd0a3ab4430e4f1084719
DOCHUNGHIEU8386 <quangngochieu266@gmail.com>
2026-02-01 19:31:39 +0700

    Update B1.java

--- B1.java
+++ B1.java
@@ -1,32 +1,44 @@
 import java.time.LocalDate;
 
-class Student{
-    private String studentId;
-    private String fullName;
-    private LocalDate dateOfBirth;
-    private double averageScore;
+class SinhVien {
+    private String maSV;
+    private String hoTen;
+    private LocalDate ngaySinh;
+    private double diemTrungBinh;
 
-    public Student(String studentId , String fullName , LocalDate dateOfBirth , double averageScore){
-        this.studentId = studentId;
-        this.fullName = fullName;
-        this.dateOfBirth = dateOfBirth;
-        this.averageScore = averageScore;
+    public SinhVien(String maSV, String hoTen, LocalDate ngaySinh, double diemTrungBinh) {
+        this.maSV = maSV;
+        this.hoTen = hoTen;
+        this.ngaySinh = ngaySinh;
+        this.diemTrungBinh = diemTrungBinh;
     }
 
-    public void displayInfo(){
-        System.out.println("Ma sinh vien : " +studentId);
-        System.out.println("Ho ten sinh vien : "+fullName);
-        System.out.println("Ngay sinh : "+dateOfBirth);
-        System.out.println("Diem trung binh : "+averageScore);
+    public void hienThiThongTin() {
+        System.out.println("Ma sinh vien: " + maSV);
+        System.out.println("Ho ten sinh vien: " + hoTen);
+        System.out.println("Ngay sinh: " + ngaySinh);
+        System.out.println("Diem trung binh: " + diemTrungBinh);
         System.out.println("------------------------------");
     }
 }
 
-public class B1{
+public class Bai1 {
     public static void main(String[] args) {
-        Student student1 = new Student("SV1" , "Ngo Quang Anh" , LocalDate.of(2003 , 3 , 19) , 8.50);
-        Student student2 = new Student("SV2" , "Anh Quang" , LocalDate.of(2006 , 2 , 12) , 10);
-        student1.displayInfo();
-        student2.displayInfo();
+        SinhVien sv1 = new SinhVien(
+                "SV001",
+                "Hieu Quang Ngoc",
+                LocalDate.of(2004, 8, 15),
+                8.7
+        );
+
+        SinhVien sv2 = new SinhVien(
+                "SV002",
+                "Tran Minh Duc",
+                LocalDate.of(2003, 12, 5),
+                7.9
+        );
+
+        sv1.hienThiThongTin();
+        sv2.hienThiThongTin();
     }
 }
