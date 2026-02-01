84aeca097ee9a6b18f813f604ee96f9915dcdacb
DOCHUNGHIEU8386 <quangngochieu266@gmail.com>
2026-02-01 19:32:22 +0700

    Update B2.java

--- B2.java
+++ B2.java
@@ -1,35 +1,42 @@
-class Account{
-    private String username;
-    private String password;
+// Lớp TaiKhoan
+class TaiKhoan {
+    private String tenDangNhap;
+    private String matKhau;
     private String email;
 
-    public Account(String username , String password , String email){
-        this.username = username;
-        this.password = password;
+    public TaiKhoan(String tenDangNhap, String matKhau, String email) {
+        this.tenDangNhap = tenDangNhap;
+        this.matKhau = matKhau;
         this.email = email;
     }
 
-    public void changePassword(String newPassword){
-        this.password = newPassword;
-        System.out.println("Doi mat khau thanh cong");
+    public void capNhatMatKhau(String matKhauMoi) {
+        this.matKhau = matKhauMoi;
+        System.out.println(">> Cap nhat mat khau thanh cong!");
     }
 
-    public void displayInfo(){
-        System.out.println("Username : "+username);
-        System.out.println("Email : "+email);
-        System.out.println("Password : ********");
-        System.out.println("---------------");
+    public void hienThiThongTin() {
+        System.out.println("Ten dang nhap : " + tenDangNhap);
+        System.out.println("Email         : " + email);
+        System.out.println("Mat khau      : ********");
+        System.out.println("--------------------------------");
     }
 }
 
-public class B2 {
-    public static void main(String[] args){
-        Account account1 = new Account("quanganh" , "190303" , "ngoquanganh2003a@gmail.com");
+// Lớp chính
+public class BaiTapTaiKhoan {
+    public static void main(String[] args) {
 
-        account1.displayInfo();
+        TaiKhoan tk = new TaiKhoan(
+                "hieuquangngoc",
+                "123456",
+                "hieuquangngoc@gmail.com"
+        );
 
-        account1.changePassword("0386878941");
+        tk.hienThiThongTin();
 
-        account1.displayInfo();
+        tk.capNhatMatKhau("newpass2026");
+
+        tk.hienThiThongTin();
     }
 }
